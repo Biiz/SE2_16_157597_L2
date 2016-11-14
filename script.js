ac72f1ad7f08f443bd7d027544ac8d10313855f5
Claudio Bizzotto <Biiz@users.noreply.github.com>
2016-11-14 09:02:24 +0100

    aggiunte funzioni
    updateStorage(); addRow(); aggiornaListe(); clearInput(); updateQuantity();

--- script.js
+++ script.js
@@ -53,3 +53,55 @@
 			alert("reached max storage!");
 		}
 	}
+
+
+/**
+ * @brief Function that update the storage quantity, invoked after the user has pressed the button "update"
+ */
+	function updateQuantity(){
+		//check the storage and the total quantity
+		quantityCount += parseInt(orderQT);
+		
+		alertQuantity();
+
+		document.getElementById("storageQT").innerHTML = parseInt(quantityCount);
+	}
+
+/**
+ * @brief function that clear the text-input inside a form
+ */
+	function clearInput(){
+		document.getElementById("inputForm").reset();
+	}
+
+/**
+ * @brief function that push the new element in the list
+ */
+	function aggiornaListe(){
+		in_cart_item.push(orderIT);
+		in_cart_quantity.push(orderQT);
+	}
+	
+/**
+ * @brief Function that insert two values in a new row af a table
+ */
+	function addRow(){
+		// Create an empty <tr> element and add it to the last position of the table:
+		var row = table.insertRow(-1);
+
+		// Insert new cells (<td> elements) at the 1st and 2nd position of the "new" <tr> element:
+		var cell1 = row.insertCell(0);
+    	var cell2 = row.insertCell(1);
+
+		// Add the value to the new cells:
+		cell1.innerHTML = orderIT;
+		cell2.innerHTML = parseInt(orderQT);
+	}
+
+/**
+ * @brief function that update the storage quantity getting the value from the text field
+ */
+	function updateStorage(){
+		storageSP = document.getElementById("storageSpace").value;
+		alertQuantity();
+	}
